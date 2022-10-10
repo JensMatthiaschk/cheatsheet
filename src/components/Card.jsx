12cabf2f5888c989def98fdbfcfdce21e31ecc88
JensMatthiaschk <jens.matthiaschk@gmail.com>
2022-10-10 16:08:55 +0200

    Add CSS Data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,18 +19,19 @@ export default function Card({ item }) {
   return (
     <>
 
-      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card w-96 bg-base-100 shadow-xl my-3">
         <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          <img src={item.img} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{item.title}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{item.description}</p>
           <div className="card-actions justify-end">
-            <a href={item.link} target='_blank'>Learn More</a>
+            <a className="badge" href={item.link} target='_blank'>Learn More</a>
           </div>
         </div>
       </div>
+
 
       {/* <Test /> */}
       {/* <Card title='Loops' content='bsdbdbsbdsb' /> */}
