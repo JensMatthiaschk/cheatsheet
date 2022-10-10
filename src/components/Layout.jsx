eea3546ebc4354a96d771783ed59d41e0b838e33
JensMatthiaschk <jens.matthiaschk@gmail.com>
2022-10-10 16:47:38 +0200

    center content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import NavBar from './NavBar'
 
 export default function Layout({ children }) {
   return (
-    <div className="container mx-auto flex flex-wrap gap-3 content-end">
+    <div className="container mx-auto flex flex-wrap gap-3 justify-center">
       <NavBar />
       {children}
     </div>
