44cf9b732fed680aa75b98a2e9e11e4c470c9f16
vlasmar <40355727+vlasmar@users.noreply.github.com>
2022-10-10 16:03:37 +0200

    add basic html data

--- src/data/htmlData.js
+++ src/data/htmlData.js
@@ -1,8 +1,51 @@
 export const htmlData = [
     {
         id: 1,
-        title: 'DOM',
-        description: 'sfdhsfhudhufhdsfhiodsfihodsofi',
-        link: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents'
+        title: 'HTML Tags',
+        description: 'Samples of the most common tags.',
+        link: 'https://www.geeksforgeeks.org/most-commonly-used-tags-in-html/',
+        img: 'https://codebrainer.azureedge.net/images/most-used-tags.jpg'
+    },
+    {
+        id: 2,
+        title: 'Blank Page',
+        description: 'The sample for an empty HTML Page',
+        link: 'https://github.com/mdn/learning-area/blob/main/html/introduction-to-html/getting-started/index.html',
+        img: 'https://prnt.sc/Esgpc8Aj82tJ'
+    },
+    {
+        id: 3,
+        title: 'Robots.txt',
+        description: 'A robots.txt file tells search engine crawlers which URLs the crawler can access on your site.',
+        link: 'https://developers.google.com/search/docs/crawling-indexing/robots/create-robots-txt',
+        img: 'https://media.geeksforgeeks.org/wp-content/uploads/gfg-24.png'
+    },
+    {
+        id: 4,
+        title: 'Head Tags',
+        description: 'Recommended tags for the header section',
+        link: 'https://www.w3schools.com/html/html_head.asp',
+        img: 'https://www.tutorialbrain.com/wp-content/uploads/2019/01/HTML-Head.jpg'
+    },
+    {
+        id: 5,
+        title: 'Attributes',
+        description: 'A HTML attribute can be part of a HTML element and is always followed by a value',
+        link: 'https://www.onlinedesignteacher.com/2016/03/html-attributes.html',
+        img: 'https://4.bp.blogspot.com/-B5vUzJXNAoE/Vuay2ygsN2I/AAAAAAAAG5o/-qOAVBa3LRkJ0fPWywYzkAcmezRAY2Rxg/s640/html-syntax.png'
+    },
+    {
+        id: 6,
+        title: 'HTML5 Page Structure',
+        description: 'As we see the page has five important parts: DOCTYPE, HTML, HEAD, BODY, TITLE',
+        link: 'https://qatechhub.com/html-page-structure-nesting/',
+        img: 'https://qatechhub.com/wp-content/uploads/2016/09/BasicHtmlStructure.png'
+    },
+    {
+        id: 7,
+        title: 'What is the .htaccess File?',
+        description: 'The .htaccess file is a powerful website file that controls high-level configuration of your website.',
+        link: 'https://ithemes.com/blog/what-is-the-htaccess-file/#:~:text=April%201%2C%202022-,The%20.,to%20edit%20server%20configuration%20files.',
+        img: 'https://ithemes.com/wp-content/uploads/2019/02/htaccess-location.jpg'
     }
 ]
